99b34bf32302b49b771c0d1e70fa4b9dd53d0dfc
kumoryo <314004856@qq.comsadf>
2012-08-21 12:14:58 +0800

    add

---
+++ TheNewGitTest/GitTest.h
@@ -0,0 +1,13 @@
+//
+//  GitTest.h
+//  TheNewGitTest
+//
+//  Created by 晓川 on 12-8-21.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GitTest : NSObject
+
+@end
---
+++ TheNewGitTest/GitTest.m
@@ -0,0 +1,13 @@
+//
+//  GitTest.m
+//  TheNewGitTest
+//
+//  Created by 晓川 on 12-8-21.
+//
+//
+
+#import "GitTest.h"
+
+@implementation GitTest
+
+@end
